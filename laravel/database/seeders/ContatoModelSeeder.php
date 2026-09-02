fa81c19c85a5c2a853a8fc855de9bd2c543d2e68
vitor-daniel015 <vi.daniel16@gmail.com>
2026-09-02 11:42:20 -0300

    feat: aula 02-09-2

--- laravel/database/seeders/ContatoModelSeeder.php
+++ laravel/database/seeders/ContatoModelSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\ContatoModel;
 
 class ContatoModelSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class ContatoModelSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $contato = new ContatoModel();
+        $contato->nome = 'Diogo';
+        $contato->email = 'Diogo@email.com';
+
     }
 }
