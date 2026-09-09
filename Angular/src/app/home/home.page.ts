005cc7504dc1805aed4105c2805c8b4737bbd7a7
vitor-daniel015 <vi.daniel16@gmail.com>
2026-09-09 12:30:42 -0300

    feat: fim da aula 09-09

--- Angular/src/app/home/home.page.ts
+++ Angular/src/app/home/home.page.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
-import { Router } from '@angular/router';
 import { CrudService } from '../services/api.service';
 
 @Component({
@@ -10,16 +8,24 @@ import { CrudService } from '../services/api.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  produtos:any;
+  produtos: any;
 
-  constructor( 
-    public crudService: CrudService
-  ){ }
+  constructor(
+    public crudService: CrudService,
+    public messageService: MessageService,
+  ) { }
 
   listarProdutos() {
     this.crudService.get('produtos').subscribe(produtos => {
       this.produtos = produtos
+      this.messageService.show('Todos os produtos foram listados')
     })
   }
-
+  removerProduto(id: any) {
+    this.crudService.delete(`produtos/${id}`).subscribe(resp => {
+      console.log(resp);
+      this.messageService.show(`O produto com id: ${id} foi removido`);
+      this.listarProdutos()
+    })
+  }
 }
